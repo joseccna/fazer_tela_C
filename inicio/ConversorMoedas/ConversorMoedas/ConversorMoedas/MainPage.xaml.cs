8a4477b90a51a3f431d317d3a43cf37b76178ce6
jose.ccna <jose.ccna@gmail.com>
2026-01-28 22:12:22 -0300

    terminado o conversor de moedas

--- inicio/ConversorMoedas/ConversorMoedas/ConversorMoedas/MainPage.xaml.cs
+++ inicio/ConversorMoedas/ConversorMoedas/ConversorMoedas/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.Gaming.Input;
+﻿using System.Globalization;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
 
 namespace ConversorMoedas
 {
@@ -12,7 +14,7 @@ namespace ConversorMoedas
 
         };
 
-        private readonly Dictionary<string, string> _cultureCurrency = new()
+        private readonly Dictionary<string, string> _cultureByCurrency = new()
         {
             {"BRL", "pt-BR"},
             {"USD", "en-US"},
@@ -22,7 +24,90 @@ namespace ConversorMoedas
         public MainPage()
         {
             InitializeComponent();
+            InitDefaults();
         }
+
+        void InitDefaults()
+        {
+            FromPicker.SelectedIndex = IndexOf(FromPicker, "BRL");
+            ToPicker.SelectedIndex = IndexOf(ToPicker, "USD");
+
+            InfoLabel.Text = "Taxas de câmbio aproximadas apenas para fins educacionais.";
+            ResultLabel.Text = string.Empty;
+
+        }
+
+        int IndexOf(Picker picker, string item) => picker.Items.IndexOf(item);
+
+        void OnInverterClicked(object? sender, EventArgs e)
+        {
+            var formIndex = FromPicker.SelectedIndex;
+            FromPicker.SelectedIndex = ToPicker.SelectedIndex;
+            ToPicker.SelectedIndex = formIndex;
+            
+        }
+
+        void OnPickerChanged(object? sender, EventArgs e)
+        {
+            ResultLabel.Text = string.Empty;
+        }
+
+
+
+
+        void OnAmountChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(AcountEntry.Text))
+            {
+                InfoLabel.Text = "Digite um valor para converter";
+            }
+
+        }
+
+        async void OnConverterClicked(object? sender, EventArgs e)
+        {
+            try
+            {
+                var from = GetFrom();
+                var to = GetTo();
+
+                if (string.IsNullOrWhiteSpace(AcountEntry.Text))
+                {
+                    await DisplayAlert("Atenção", "Informe um número valido", "OK");
+                    return;
+                }
+                if (!decimal.TryParse(AcountEntry.Text,NumberStyles.Number, CultureInfo.CurrentCulture, out var amount) || amount < 0)
+                {
+                    await DisplayAlert("Atenção", "valor invalido", "OK");
+                    return;
+                }
+
+                var result = Convert(from, to, amount);
+
+                var culture = new CultureInfo(_cultureByCurrency[to]);
+
+                var formatted = result.ToString("C", culture);
+
+                ResultLabel.Text = $"{amount} {from} = {formatted}";
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", $"Falha ao converter:{ex.Message}", "OK");
+            }
+
+        }
+
+        decimal Convert(string from, string to, decimal amount)
+        {
+            if (from == to) return amount;
+            var brl = amount * _toBRL[from];
+            var result = brl / _toBRL[to];
+            return decimal.Round(result, 4); 
+      
+        }
+        string? GetFrom() => FromPicker.SelectedIndex >= 0 ? FromPicker.Items[FromPicker.SelectedIndex] : null;
+        string? GetTo() => ToPicker.SelectedIndex >= 0 ? ToPicker.Items[ToPicker.SelectedIndex] : null;
 
 
 
--- inicio/ConversorMoedas/ConversorMoedas/ConversorMoedas/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/ConversorMoedas.AssemblyInfo.cs
+++ inicio/ConversorMoedas/ConversorMoedas/ConversorMoedas/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/ConversorMoedas.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConversorMoedas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7c49394c7cff5cc097d41f59b915fd6ae04e751e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+57f66b8855ef6b393ae450a1129010f223e09b42")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConversorMoedas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConversorMoedas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
