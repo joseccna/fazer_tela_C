6e511bd505c24cfa324ed99e87f9c9df08dcb2a7
jose.ccna <jose.ccna@gmail.com>
2026-01-29 21:54:40 -0300

    aplicativo com três telas

---
+++ inicio/DuasTelas/NavegacaoEntreTelas/NavegacaoEntreTelas/HomePage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavegacaoEntreTelas
+{
+    partial class HomePage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnEntrarClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("HomePage");
+        }
+
+        async void OnCadastrarClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("Cadastro");
+
+        }
+
+
+
+        async void OnsairClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//MainPage");
+        }
+
+
+    }
+}
